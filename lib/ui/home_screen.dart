f2cbb043497247561a20897bcbbdb29f8dc2a7fa
Kaykobad <kaykobadreza@gmail.com>
2021-01-24 12:32:41 +0600

    Refactor homepage

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -13,78 +13,102 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(StringConstants.APP_NAME),
-        centerTitle: true,
+      appBar: _getAppBar(),
+      body: _getBody(),
+      bottomSheet: _getBottomSheet(context),
+    );
+  }
+
+  AppBar _getAppBar() {
+    return AppBar(
+      title: Text(StringConstants.APP_NAME),
+      centerTitle: true,
+    );
+  }
+
+  Container _getBottomSheet(BuildContext context) {
+    return Container(
+      color: Theme.of(context).primaryColor,
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          _getRadioStationImage(),
+          _getRadioStationName(),
+          _getPlayPauseButton(),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(bottom: 100),
-        child: Container(
-          child: Center(
-            child: BlocBuilder<RadioBloc, RadioState>(builder: (context, state) {
-              if (state is PausedState) {
-                return IdleIndicatorWidget(color: Theme.of(context).accentColor);
-              } else if (state is PlayingState) {
-                return Loading(
-                  indicator: LineScalePulseOutIndicator(),
-                  size: 100,
-                  color: Theme.of(context).accentColor,
-                );
-              } else {
-                throw Exception('Unknown state of bloc');
-              }
-            }),
-          ),
-        ),
+    );
+  }
+
+  Padding _getPlayPauseButton() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: BlocBuilder<RadioBloc, RadioState>(
+        builder: (context, state) {
+          if (state is PausedState) {
+            return IconButton(
+              icon: Icon(
+                Icons.play_arrow,
+                size: 32,
+              ),
+              onPressed: () {
+                BlocProvider.of<RadioBloc>(context).add(PlayEvent(StringConstants.planetRockUrl));
+              },
+            );
+          } else {
+            return IconButton(
+              icon: Icon(
+                Icons.pause,
+                size: 32,
+              ),
+              onPressed: () {
+                BlocProvider.of<RadioBloc>(context).add(PauseEvent());
+              },
+            );
+          }
+        },
       ),
-      bottomSheet: Container(
-        color: Theme.of(context).primaryColor,
-        height: 100,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Container(
-                height: 50,
-                width: 50,
-                child: Image.asset(StringConstants.planetRockImage),
-              ),
-            ),
-            Text(
-              'Planet Rock',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: BlocBuilder<RadioBloc, RadioState>(
-                builder: (context, state) {
-                  if (state is PausedState) {
-                    return IconButton(
-                      icon: Icon(
-                        Icons.play_arrow,
-                        size: 32,
-                      ),
-                      onPressed: () {
-                        context.bloc<RadioBloc>().add(PlayEvent(StringConstants.planetRockUrl));
-                      },
-                    );
-                  } else {
-                    return IconButton(
-                      icon: Icon(
-                        Icons.pause,
-                        size: 32,
-                      ),
-                      onPressed: () {
-                        context.bloc<RadioBloc>().add(PauseEvent());
-                      },
-                    );
-                  }
-                },
-              ),
-            )
-          ],
+    );
+  }
+
+  Text _getRadioStationName() {
+    return Text(
+      'Planet Rock',
+      style: TextStyle(fontWeight: FontWeight.bold),
+    );
+  }
+
+  Padding _getRadioStationImage() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        height: 50,
+        width: 50,
+        child: Image.asset(StringConstants.planetRockImage),
+      ),
+    );
+  }
+
+  Padding _getBody() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 100),
+      child: Container(
+        child: Center(
+          child: BlocBuilder<RadioBloc, RadioState>(builder: (context, state) {
+            if (state is PausedState) {
+              return IdleIndicatorWidget(color: Theme.of(context).accentColor);
+            } else if (state is PlayingState) {
+              return Loading(
+                indicator: LineScalePulseOutIndicator(),
+                size: 100,
+                color: Theme.of(context).accentColor,
+              );
+            } else {
+              throw Exception('Unknown state of bloc');
+            }
+          }),
         ),
       ),
     );
